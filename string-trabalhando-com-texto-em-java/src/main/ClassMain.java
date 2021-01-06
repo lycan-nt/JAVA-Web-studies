8ff61a94df486b567982a17cf6658d141c65f5a6
Felipe D. Santos <lycan.felipe@gmail.com>
2021-01-06 19:24:42 -0300

    Working with String

---
+++ string-trabalhando-com-texto-em-java/src/main/ClassMain.java
@@ -0,0 +1,17 @@
+package main;
+
+public class ClassMain {
+
+	public static void main(String[] args) {
+		String myString = new String();
+		myString = "JAVA";
+		String myString2 = "JAVA";
+		String myString3 = new String("JAVA");
+		String myString4 = "";
+		
+		char[] myChars = new char[] {'J', 'A', 'V', 'A'};
+		String myString5 = new String(myChars);
+
+	}
+
+}
