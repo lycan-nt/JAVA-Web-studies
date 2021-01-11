e2a800ed275703a45efd8ed1ae0726c99af34691
Felipe D. Santos <lycan.felipe@gmail.com>
2021-01-11 20:44:48 -0300

    Strings

---
+++ String/src/main/String.java
@@ -0,0 +1,33 @@
+package main;
+
+public class String {
+
+	public static void main(String[] args) {
+		//Creat String
+		String nome = "Felipe D. Santos";
+		
+		String name = new String ("Felipe D. Santos");
+		
+		"Qualquer text".length();
+		
+		String text;
+	}
+	
+	public static void testeNames()
+	{
+		String name = "Arthur";
+		String app = name;
+		
+		if (name == app)
+			System.out.println("Ok");
+		
+		String name1 = "carlor";
+		String name2 = "carla";
+		
+		if (nome1.equals(nome2))
+			System.out.println("Os nomes são iguais");
+		else
+			System.out.println("Nõ são iguais");
+	}
+
+}
