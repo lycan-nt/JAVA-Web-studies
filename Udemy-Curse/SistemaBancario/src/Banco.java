a78c2989654210212ddb4d4872e11626b257b32b
lycan-nt <lycan.felipe@gmail.com>
2021-02-24 05:40:06 -0300

    Construictors

---
+++ Udemy-Curse/SistemaBancario/src/Banco.java
@@ -0,0 +1,8 @@
+
+public class Banco {
+	public static void main(String args[]) {
+		Correntista c = new Correntista(1, "Felipe");
+		
+		System.out.println("Name: " + c.getNome());
+	}
+}
--- Udemy-Curse/SistemaBancario/src/Correntista.java
+++ Udemy-Curse/SistemaBancario/src/Correntista.java
@@ -3,6 +3,18 @@ public class Correntista {
 	private int codigo;
 	private String nome;
 	
+	public Correntista () 
+	{
+		this.codigo = 0;
+		this.nome = "";
+	}
+	
+	public Correntista (int codigo, String nome) 
+	{
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+	
 	public int getCodigo() 
 	{
 		return codigo;
