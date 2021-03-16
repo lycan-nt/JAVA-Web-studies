57378fa3901a81a63dcb34b60d6d2c2153fb5116
lycan-nt <lycan.felipe@gmail.com>
2021-03-16 06:13:56 -0300

    Method static

--- Udemy-Curse/SistemaBancario/src/Banco.java
+++ Udemy-Curse/SistemaBancario/src/Banco.java
@@ -1,8 +1,12 @@
 
 public class Banco {
 	public static void main(String args[]) {
-		Correntista c = new Correntista(1, "Felipe");
+		Correntista c = new Correntista("Felipe");
+
+		Correntista c2 = new Correntista("Marcella");
 		
-		System.out.println("Name: " + c.getNome());
+		System.out.println("Name: " + c.getNome() + " " + "Cod: " + c.getCodigo());
+
+		System.out.println("Name: " + c2.getNome() + " " + "Cod: " + c2.getCodigo());
 	}
 }
--- Udemy-Curse/SistemaBancario/src/Correntista.java
+++ Udemy-Curse/SistemaBancario/src/Correntista.java
@@ -1,28 +1,19 @@
 
 public class Correntista {
+	private static int proxCod = 1;
 	private int codigo;
 	private String nome;
 	
-	public Correntista () 
+	public Correntista (String nome) 
 	{
-		this.codigo = 0;
-		this.nome = "";
-	}
-	
-	public Correntista (int codigo, String nome) 
-	{
-		this.codigo = codigo;
+		this.codigo = proxCod;
+		proxCod++;
 		this.nome = nome;
 	}
 	
 	public int getCodigo() 
 	{
 		return codigo;
-	}
-	
-	public void setCodigo (int codigo)
-	{
-		this.codigo = codigo;
 	}
 
 	public String getNome() {
