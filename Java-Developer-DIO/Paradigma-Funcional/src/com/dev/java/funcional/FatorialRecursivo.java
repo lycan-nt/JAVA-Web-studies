d7989fd8860d218253543b54e3c78b248f6fd190
Felipe D. Santos <lycan.felipe@gmail.com>
2021-07-27 22:27:19 -0300

    Recursividade

---
+++ Java-Developer-DIO/Paradigma-Funcional/src/com/dev/java/funcional/FatorialRecursivo.java
@@ -0,0 +1,19 @@
+package com.dev.java.funcional;
+
+public class FatorialRecursivo {
+    public static void main(String[] args) {
+        System.out.println(fatorial(5));
+    }
+
+    public static int fatorial(int value)
+    {
+        if (value == 1)
+        {
+            return value;
+        }
+        else
+        {
+            return value * fatorial((value - 1));
+        }
+    }
+}
