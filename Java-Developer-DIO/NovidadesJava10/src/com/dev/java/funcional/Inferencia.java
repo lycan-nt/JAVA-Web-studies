de9415b889a762f7d7361ec2789b83358ffa1290
Felipe D. Santos <lycan.felipe@gmail.com>
2021-08-02 20:58:23 -0300

    Releases

---
+++ Java-Developer-DIO/NovidadesJava10/src/com/dev/java/funcional/Inferencia.java
@@ -0,0 +1,19 @@
+package com.dev.java.funcional;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.stream.Collectors;
+
+public class Inferencia {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://google.com");
+        URLConnection urlConnection = url.openConnection();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+        System.out.println(bufferedReader.lines().collect(Collectors.joining()).replace(">", ">\n"));
+    }
+}
