5ba74611c23863c388f6d7a5697fd6905a226b51
Felipe D. Santos <lycan.felipe@gmail.com>
2021-08-02 21:27:06 -0300

    Release

--- Java-Developer-DIO/NovidadesJava10/src/com/dev/java/funcional/Inferencia.java
+++ Java-Developer-DIO/NovidadesJava10/src/com/dev/java/funcional/Inferencia.java
@@ -10,10 +10,38 @@ import java.util.stream.Collectors;
 
 public class Inferencia {
     public static void main(String[] args) throws IOException {
-        URL url = new URL("https://google.com");
-        URLConnection urlConnection = url.openConnection();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        printName("Felipe", " D. Santos");
+        printSoma(10, 10, 50, 30);
+    }
+
+    private void getConnectionURL() throws IOException
+    {
+        var url = new URL("https://google.com");
+        var urlConnection = url.openConnection();
+        var bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 
         System.out.println(bufferedReader.lines().collect(Collectors.joining()).replace(">", ">\n"));
+
+    }
+
+    public static void printName(String nome, String sobreNome)
+    {
+        var nomeCompleto = nome + sobreNome;
+        System.out.println(nomeCompleto);
+    }
+
+    public static void printSoma(int... numeros)
+    {
+        var soma = 0;
+
+        if (numeros.length > 0)
+        {
+            for (var numero : numeros)
+            {
+                soma += numero;
+            }
+
+            System.out.println("Result: " + soma);
+        }
     }
 }
