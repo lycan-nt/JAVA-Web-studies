00f4c513b797a9c7e87b088f08d0bc15a81c2550
Felipe D. Santos <lycan.felipe@gmail.com>
2021-08-15 18:42:18 -0300

    Drivers de conexão

--- Java-Developer-DIO/NovidadesJava11/src/com/dev/java/funcional/Main.java
+++ Java-Developer-DIO/NovidadesJava11/src/com/dev/java/funcional/Main.java
@@ -5,12 +5,27 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        connect();
+    public static void main(String[] args) throws IOException, ParseException {
+        NumberFormat nformat = NumberFormat.getInstance(Locale.GERMAN);
+
+        int qtdPositiva = 0;
+        for (int i = 0; i < 6; i++)
+        {
+            Scanner leitor = new Scanner(System.in);
+
+            double x =  Double.valueOf(leitor.nextLine());
+            qtdPositiva += x > 0 ? 1 : 0;
+        }
+        System.out.println(qtdPositiva + " valores positivos");
     }
+
 
     private static void connect() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
