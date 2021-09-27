38b2d163f5a99073c5f72e1e10d0f2017e249149
Felipe D. Santos <lycan.felipe@gmail.com>
2021-09-27 13:10:08 -0300

    Revisão

---
+++ Revisao01/src/main/java/study/stream/Pessoa.java
@@ -0,0 +1,36 @@
+package study.stream;
+
+public class Pessoa {
+
+    private String nome;
+    private String sobreNome;
+    private int idade;
+
+    public String getNome()
+    {
+        return this.nome;
+    }
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    public String getSobreNome()
+    {
+        return this.sobreNome;
+    }
+    public void setSobreNome(String sobreNome)
+    {
+        this.sobreNome = sobreNome;
+    }
+
+    public int getIdade()
+    {
+        return this.idade;
+    }
+    public void setIdade(int Idade)
+    {
+        this.idade = idade;
+    }
+
+}
