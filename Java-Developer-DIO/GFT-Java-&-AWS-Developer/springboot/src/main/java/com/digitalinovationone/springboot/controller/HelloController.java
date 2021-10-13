aff2d026ccad05ce7bec2f850520ed17699dfcd4
Felipe D. Santos <lycan.felipe@gmail.com>
2021-10-13 20:09:19 -0300

    Hello DIO

---
+++ Java-Developer-DIO/GFT-Java-&-AWS-Developer/springboot/src/main/java/com/digitalinovationone/springboot/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.digitalinovationone.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloMessage()
+    {
+        return "Hello, DIO!";
+    }
+}
