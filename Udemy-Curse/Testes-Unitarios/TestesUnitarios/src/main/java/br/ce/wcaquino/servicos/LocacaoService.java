7e8ee8ca9504b857d75176a80d7ebc6136726c7d
Felipe D. Santos <lycan.felipe@gmail.com>
2021-10-26 21:32:31 -0300

    Organização

--- Udemy-Curse/Testes-Unitarios/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ Udemy-Curse/Testes-Unitarios/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -32,27 +32,4 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	@Test
-	public void teste() {
-		//Cenário
-		Usuario usuario = new Usuario();
-		usuario.setNome("Felipe D. Santos");
-		
-		Filme filme = new Filme();
-		filme.setNome("Guardiões");
-		filme.setPrecoLocacao(10.50);
-		filme.setEstoque(1);
-		
-		//Acão
-		Locacao locacao = alugarFilme(usuario, filme);
-		
-		//Verificação
-		System.out.println("=====Teste de locação=====");
-		System.out.println("Filme: " + locacao.getFilme().getNome());
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-		Assert.assertTrue(locacao.getValor() == 10.50);
-		System.out.println("Locador: " + locacao.getUsuario().getNome());
-		System.out.println("================");
-	}
 }
---
+++ Udemy-Curse/Testes-Unitarios/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,39 @@
+package br.ce.wcaquino.servicos;
+
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.servicos.LocacaoService;
+import br.ce.wcaquino.utils.DataUtils;
+
+public class LocacaoServiceTest {
+	@Test
+	public void teste() {
+		//Cenário
+		Usuario usuario = new Usuario();
+		usuario.setNome("Felipe D. Santos");
+		
+		Filme filme = new Filme();
+		filme.setNome("Guardiões");
+		filme.setPrecoLocacao(10.50);
+		filme.setEstoque(1);
+		
+		//Acão
+		Locacao locacao = LocacaoService.alugarFilme(usuario, filme);
+		
+		//Verificação
+		System.out.println("=====Teste de locação=====");
+		System.out.println("Filme: " + locacao.getFilme().getNome());
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		Assert.assertTrue(locacao.getValor() == 10.50);
+		System.out.println("Locador: " + locacao.getUsuario().getNome());
+		System.out.println("================");
+	}
+}
