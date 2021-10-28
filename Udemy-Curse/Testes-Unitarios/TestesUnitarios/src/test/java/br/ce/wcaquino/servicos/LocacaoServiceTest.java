5c40f77798930c2b77bffd02d5c5ec2c1948f3d2
Felipe D. Santos <lycan.felipe@gmail.com>
2021-10-28 20:41:41 -0300

    Formas de se dividir um teste

--- Udemy-Curse/Testes-Unitarios/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ Udemy-Curse/Testes-Unitarios/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,19 +1,22 @@
 package br.ce.wcaquino.servicos;
 
-
 import java.util.Date;
-
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.servicos.LocacaoService;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
+	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
 	@Test
 	public void teste() {
 		//Cenário
@@ -29,12 +32,9 @@ public class LocacaoServiceTest {
 		Locacao locacao = LocacaoService.alugarFilme(usuario, filme);
 		
 		//Verificação
-		System.out.println("=====Teste de locação=====");
-		System.out.println("Filme: " + locacao.getFilme().getNome());
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-		Assert.assertThat(locacao.getValor(), CoreMatchers.is(10.50));
-		System.out.println("Locador: " + locacao.getUsuario().getNome());
-		System.out.println("================");
+		error.checkThat(locacao.getValor(), CoreMatchers.is(10.50));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.is(true));
+
 	}
 }
