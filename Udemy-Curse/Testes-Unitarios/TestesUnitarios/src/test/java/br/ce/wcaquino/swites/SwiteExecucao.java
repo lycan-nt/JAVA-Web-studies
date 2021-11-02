e5652a85c47c479acfc569a3eea9fe7e6289092d
Felipe D. Santos <lycan.felipe@gmail.com>
2021-11-02 12:52:46 -0300

    Suite

---
+++ Udemy-Curse/Testes-Unitarios/TestesUnitarios/src/test/java/br/ce/wcaquino/swites/SwiteExecucao.java
@@ -0,0 +1,21 @@
+package br.ce.wcaquino.swites;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import br.ce.wcaquino.servicos.CalculadoraTest;
+import br.ce.wcaquino.servicos.CalculoValorLocacaoTest;
+import br.ce.wcaquino.servicos.LocacaoServiceTest;
+
+//@RunWith(Suite.class)
+@SuiteClasses({
+	CalculadoraTest.class,
+	CalculoValorLocacaoTest.class,
+	LocacaoServiceTest.class
+})
+public class SwiteExecucao {
+
+}
