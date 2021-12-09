2af4cc5686a40c9c5f645d495926b2db27d6f825
Felipe D. Santos <lycan.felipe@gmail.com>
2021-12-08 23:40:28 -0300

    Polimorfismo

--- Java-Developer-DIO/TQI-Java-Developer/Introducao-a-poo/src/com/dev/poo/model/Pessoa.java
+++ Java-Developer-DIO/TQI-Java-Developer/Introducao-a-poo/src/com/dev/poo/model/Pessoa.java
@@ -1,9 +1,34 @@
 package com.dev.poo.model;
 
 public class Pessoa {
+    private static final int TAMANHO_CPF = 11;
+    private static final int TAMANHO_CNPJ = 14;
+
     public enum TipoPessoa{FISICA, JURIDICA}
     public Integer codigo;
     public String nome;
-    public String documento;
+    private String documento;
     public TipoPessoa tipo;
+
+    public String getDocumento(){
+        return this.documento;
+    }
+    public void setDocumento(String documento){
+        if (documento == null || documento.isEmpty()){
+            throw new RuntimeException("O numero do cocumento não pode ser nulo ou vazio");
+        }
+        if (documento.length() == TAMANHO_CPF){
+            setDocumento(documento, TipoPessoa.FISICA);
+        } else if(documento.length() == TAMANHO_CNPJ){
+            setDocumento(documento, TipoPessoa.JURIDICA);
+        } else {
+            throw new RuntimeException("Documento invalido para pessoa FISICA/JURIDICA");
+        }
+    }
+
+    private void setDocumento(String documento, TipoPessoa tipoPessoa){
+        this.tipo = TipoPessoa.FISICA;
+        this.documento = documento;
+    }
+
 }
