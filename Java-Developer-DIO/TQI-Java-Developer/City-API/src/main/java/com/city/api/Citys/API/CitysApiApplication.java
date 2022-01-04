1e366a27ed26ff5bbc46910279cd7528aed830d7
Felipe D. Santos <lycan.felipe@gmail.com>
2022-01-03 22:07:45 -0300

    Init project city-api

---
+++ Java-Developer-DIO/TQI-Java-Developer/City-API/src/main/java/com/city/api/Citys/API/CitysApiApplication.java
@@ -0,0 +1,13 @@
+package com.city.api.Citys.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitysApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitysApiApplication.class, args);
+	}
+
+}
