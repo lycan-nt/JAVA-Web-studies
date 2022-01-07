fc82a64c0871e9344e248f733fedcb89980bab9d
Felipe D. Santos <lycan.felipe@gmail.com>
2022-01-06 21:52:46 -0300

    Pagenator

--- Java-Developer-DIO/TQI-Java-Developer/City-API/src/main/java/com/city/api/Citys/API/CountryResource.java
+++ Java-Developer-DIO/TQI-Java-Developer/City-API/src/main/java/com/city/api/Citys/API/CountryResource.java
@@ -2,11 +2,11 @@ package com.city.api.Citys.API;
 
 import com.city.api.Citys.API.countries.Country;
 import com.city.api.Citys.API.repository.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
-
 
 @RestController
 @RequestMapping("/countries")
@@ -19,8 +19,8 @@ public class CountryResource {
     }
 
     @GetMapping
-    public List<Country> countries(){
-        return repository.findAll();
+    public Page<Country> countries(Pageable page){
+        return repository.findAll(page);
     }
 
 }
