91eef725c07a8467d2f2cb5eac18f5345dc42767
Felipe D. Santos <lycan.felipe@gmail.com>
2022-01-07 05:58:31 -0300

    Init Project Bookstar

---
+++ Projects/BoockStar/boockstar/src/main/java/com/boock/star/BoockstarApplication.java
@@ -0,0 +1,13 @@
+package com.boock.star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoockstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoockstarApplication.class, args);
+	}
+
+}
