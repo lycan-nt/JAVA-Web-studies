a9d95b671576d0cc4b049b44e1c248d845a7a544
Felipe D. Santos <lycan.felipe@gmail.com>
2022-01-12 05:22:47 -0300

    Finaly class domany

--- Projects/BoockStar/boockstar/src/main/java/com/boock/star/doman/Categoria.java
+++ Projects/BoockStar/boockstar/src/main/java/com/boock/star/doman/Categoria.java
@@ -1,13 +1,27 @@
 package com.boock.star.doman;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
-public class Categoria {
+@Entity
+public class Categoria implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	private String nome;
 	private String descricao;
+	
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<>();
 	
 	public Categoria() {
--- Projects/BoockStar/boockstar/src/main/java/com/boock/star/doman/Livro.java
+++ Projects/BoockStar/boockstar/src/main/java/com/boock/star/doman/Livro.java
@@ -1,12 +1,27 @@
 package com.boock.star.doman;
 
+import java.io.Serializable;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
-public class Livro {
+@Entity
+public class Livro implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String titulo;
 	private String nomeAutor;
 	private String texto;
+	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
