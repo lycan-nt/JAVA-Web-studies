ae261005ca2e681f979640a24ece02d788c05aaa
Felipe D. Santos <lycan.felipe@gmail.com>
2022-01-11 21:03:10 -0300

    getById

--- Java-Developer-DIO/TQI-Java-Developer/City-API/src/main/java/com/city/api/Citys/API/CountryResource.java
+++ Java-Developer-DIO/TQI-Java-Developer/City-API/src/main/java/com/city/api/Citys/API/CountryResource.java
@@ -4,9 +4,13 @@ import com.city.api.Citys.API.countries.Country;
 import com.city.api.Citys.API.repository.CountryRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -21,6 +25,16 @@ public class CountryResource {
     @GetMapping
     public Page<Country> countries(Pageable page){
         return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getCountry(@PathVariable long id) {
+        Optional<Country> optionalCountry = repository.findById(id);
+        if(optionalCountry.isPresent()){
+            return ResponseEntity.ok().body(optionalCountry.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
