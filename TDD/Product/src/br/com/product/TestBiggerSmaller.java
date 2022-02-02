26989b45df5cb9007f9a73caab6d61759ad98d35
Felipe D. Santos <lycan.felipe@gmail.com>
2022-02-01 22:41:47 -0300

    Unit Test JustOneProduct and AscendingOrder

--- TDD/Product/src/br/com/product/TestBiggerSmaller.java
+++ TDD/Product/src/br/com/product/TestBiggerSmaller.java
@@ -19,4 +19,30 @@ public class TestBiggerSmaller {
 		Assert.assertEquals("Jogo de pratos", algoritimo.getSmaller().getName());
 	}
 	
+	@Test
+	public void ascendingOrder() {
+		ShoppingCart cart = new ShoppingCart();
+		cart.toAdd(new Product("Jogo de pratos", 70.0));
+		cart.toAdd(new Product("Liquidificador", 250.0));
+		cart.toAdd(new Product("Geladeira", 450.0));
+
+		BiggerSmaller algoritimo = new BiggerSmaller();
+		algoritimo.meet(cart);
+		
+		Assert.assertEquals("Geladeira", algoritimo.getBigger().getName());
+		Assert.assertEquals("Jogo de pratos", algoritimo.getSmaller().getName());
+	}
+	
+	@Test
+	public void justOneProduct() {
+		ShoppingCart cart = new ShoppingCart();
+		cart.toAdd(new Product("Geladeira", 450.0));
+		
+		BiggerSmaller algoritimo = new BiggerSmaller();
+		algoritimo.meet(cart);
+		
+		Assert.assertEquals("Geladeira", algoritimo.getSmaller().getName());
+		Assert.assertEquals("Geladeira", algoritimo.getBigger().getName());
+	}
+	
 }
