dba31a395da885fded27caf89c491a4b30f567c7
Felipe D. Santos <lycan.felipe@gmail.com>
2022-02-03 22:57:54 -0300

    TDD

--- TDD/RomanNumeral/src/br/com/roman/RomanNumberConvert.java
+++ TDD/RomanNumeral/src/br/com/roman/RomanNumberConvert.java
@@ -1,13 +1,33 @@
 package br.com.roman;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumberConvert {
+	
+	private Map<Character, Integer> table = new HashMap<Character, Integer>() {{
+		put('I', 1);
+		put('V', 5);
+		put('X', 10);
+		put('L', 50);
+		put('C', 100);
+		put('D', 500);
+		put('M', 1000);
+	}};
 
 	public int convert(String romanNumber) {
-		if (romanNumber.equals("I"))
-			return 1;
-		else if (romanNumber.equals("V"))
-			return 5;
-		return 0;
+		int accumulator = 0;
+		int lastNeighborRight = 0;
+		for (int i = romanNumber.length() - 1; i >= 0; i--) {
+			int current = table.get(romanNumber.charAt(i));
+			int multiplier = 1;
+			if (current < lastNeighborRight) {
+				multiplier = -1;
+			}
+			accumulator += current * multiplier;
+			lastNeighborRight = current;
+		}
+		return  accumulator;
 	}
 
 }
--- TDD/RomanNumeral/src/br/com/roman/TestRomanNumberConverter.java
+++ TDD/RomanNumeral/src/br/com/roman/TestRomanNumberConverter.java
@@ -19,4 +19,32 @@ public class TestRomanNumberConverter {
 		assertEquals(5, number);
 	}
 	
+	@Test
+	public void mustUnderstandTwoSymbolsAsII() {
+		RomanNumberConvert roman = new RomanNumberConvert();
+		int number = roman.convert("II");
+		assertEquals(2, number);
+	}
+	
+	@Test
+	public void mustUnderstandFourSymbolAsXXII() {
+		RomanNumberConvert roman = new RomanNumberConvert();
+		int number = roman.convert("XXII");
+		assertEquals(22, number);
+	}
+	
+	@Test
+	public void mustUnderstandNumbersAsIX() {
+		RomanNumberConvert roman = new RomanNumberConvert();
+		int number = roman.convert("IX");
+		assertEquals(9, number);
+	}
+	
+	@Test
+	public void mustUnderstandComplexNumbersAsXXIV() {
+		RomanNumberConvert roman = new RomanNumberConvert();
+		int number = roman.convert("XXIV");
+		assertEquals(24, number);
+	}
+	
 }
