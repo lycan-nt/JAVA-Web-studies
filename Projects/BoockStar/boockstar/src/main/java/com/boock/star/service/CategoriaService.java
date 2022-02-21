8e42acfe5d979d83fa300fed65c039be10187116
Felipe D. Santos <lycan.felipe@gmail.com>
2022-02-21 06:13:36 -0300

    Post for Categoria

--- Projects/BoockStar/boockstar/src/main/java/com/boock/star/service/CategoriaService.java
+++ Projects/BoockStar/boockstar/src/main/java/com/boock/star/service/CategoriaService.java
@@ -25,5 +25,10 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repository.findAll();
 	}
+	
+	public Categoria create(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
 
 }
