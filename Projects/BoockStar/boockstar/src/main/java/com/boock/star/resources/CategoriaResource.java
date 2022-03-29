b6116473737d77d1853ffd39b64c4b143cd4ecb9
Felipe D. Santos <lycan.felipe@gmail.com>
2022-03-29 06:09:34 -0300

    Anotation @Cross

--- Projects/BoockStar/boockstar/src/main/java/com/boock/star/resources/CategoriaResource.java
+++ Projects/BoockStar/boockstar/src/main/java/com/boock/star/resources/CategoriaResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.boock.star.doman.Categoria;
 import com.boock.star.dtos.CategoriaDTO;
 import com.boock.star.service.CategoriaService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
--- Projects/BoockStar/boockstar/src/main/java/com/boock/star/resources/LivroResource.java
+++ Projects/BoockStar/boockstar/src/main/java/com/boock/star/resources/LivroResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,6 +25,7 @@ import com.boock.star.doman.Livro;
 import com.boock.star.dtos.LivroDTO;
 import com.boock.star.service.LivroService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/livros")
 public class LivroResource {
