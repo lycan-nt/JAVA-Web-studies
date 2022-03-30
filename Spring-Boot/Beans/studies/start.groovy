60c7d6875103cacb6b28c0d6afcfa53b4259220a
Felipe D. Santos <lycan.felipe@gmail.com>
2022-03-29 21:50:04 -0300

    Study Beans

---
+++ Spring-Boot/Beans/studies/start.groovy
@@ -0,0 +1,5 @@
+@Grab("org.beans:spring-boot-starter-actuator:0.5.0.BUILD-SNAPSHOT")
+@Grab("org.beans:spring-boot-starter-web:0.5.0.BUILD-SNAPSHOT")
+@ComponentScan("org.beans.sample.service")
+class Start {
+}
