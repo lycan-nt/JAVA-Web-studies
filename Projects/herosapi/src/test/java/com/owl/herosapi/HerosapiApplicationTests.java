77b7e15bf72bf2520a5ca86c615b46a96cc423a7
Felipe D. Santos <lycan.felipe@gmail.com>
2022-04-25 22:04:04 -0300

    Heros API for study api reactive

---
+++ Projects/herosapi/src/test/java/com/owl/herosapi/HerosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owl.herosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
