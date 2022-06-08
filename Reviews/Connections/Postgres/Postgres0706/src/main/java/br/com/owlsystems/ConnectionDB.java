dcac2a241a811203ae86b3fad67e92706a6e830c
Felipe D. Santos <lycan.felipe@gmail.com>
2022-06-07 21:44:32 -0300

    Connection in postgres

---
+++ Reviews/Connections/Postgres/Postgres0706/src/main/java/br/com/owlsystems/ConnectionDB.java
@@ -0,0 +1,25 @@
+package br.com.owlsystems;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+	
+	public static Connection getConnection() {
+		Connection con = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/review0706", "postgres", "131216");
+			System.out.println("Conectado com sucesso!");
+		} catch (SQLException e) {
+			System.out.println("Falha na conexão com o DB");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.out.println("Falha no Driver do Postgresql");
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+}
---
+++ Reviews/Connections/Postgres/Postgres0706/src/main/java/br/com/owlsystems/test/TestConnection.java
@@ -0,0 +1,11 @@
+package br.com.owlsystems.test;
+
+import br.com.owlsystems.ConnectionDB;
+
+public class TestConnection {
+
+	public static void main(String[] args) {
+		ConnectionDB.getConnection();
+	}
+
+}
