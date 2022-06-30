fa5697be5cdc08960e61bf5b492ac037f5fdb6b3
Felipe D. Santos <lycan.felipe@gmail.com>
2022-06-29 21:25:45 -0300

    Connect redis and created instace RedisList

--- Reviews/Connections/Redis/redis2906/redis2906/src/com/owl/instances/RedisListJava.java
+++ Reviews/Connections/Redis/redis2906/redis2906/src/com/owl/instances/RedisListJava.java
@@ -1,4 +1,19 @@
 package com.owl.instances;
 
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+
 public class RedisListJava {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("localhost", 6379);
+        System.out.println("Connection to server sucessfully");
+        jedis.lpush("list", "Redis");
+        jedis.lpush("list", "Mongodb");
+        jedis.lpush("list", "Mysql");
+        List<String> list = jedis.lrange("list", 0, 1);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("Stored string in redis::" + list.get(i));
+        }
+    }
 }
