52b550aef0d38c3f4689a78e575b8631fe995c5b
Felipe D. Santos <lycan.felipe@gmail.com>
2022-07-01 06:48:56 -0300

    Encode password using Spring Security

--- Projects/springngblog/src/main/java/com/owl/springngblog/service/AuthService.java
+++ Projects/springngblog/src/main/java/com/owl/springngblog/service/AuthService.java
@@ -4,6 +4,7 @@ import com.owl.springngblog.dto.RegisterRequest;
 import com.owl.springngblog.model.User;
 import com.owl.springngblog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +12,17 @@ public class AuthService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
