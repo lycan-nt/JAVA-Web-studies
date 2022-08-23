4d4edc6b738e90468c2b70137af6d75c580b14fe
Felipe D. Santos <lycan.felipe@gmail.com>
2022-08-22 21:50:03 -0300

    API

---
+++ Projects/parking-control/src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -0,0 +1,15 @@
+package com.api.parkingcontrol.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TB_PARKING_SPOT")
+public class ParkingSpotModel implements Serializable {
+
+    private static final long serialVersionID = 1L;
+
+    
+
+}
