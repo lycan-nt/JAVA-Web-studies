5aa2ed9370961f209537f1ada9343f668ae7f7ed
Felipe D. Santos <lycan.felipe@gmail.com>
2022-08-23 21:52:43 -0300

    SpringBoot API

---
+++ Projects/parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,18 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+
+
+}
