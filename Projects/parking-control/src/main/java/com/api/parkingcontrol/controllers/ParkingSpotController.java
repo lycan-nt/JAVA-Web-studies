664d879076f55d7c069c289a92ad5f96f359acaa
Felipe D. Santos <lycan.felipe@gmail.com>
2022-08-31 06:03:19 -0300

    Update

--- Projects/parking-control/src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ Projects/parking-control/src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -71,4 +71,19 @@ public class ParkingSpotController {
         return ResponseEntity.status(HttpStatus.OK).body("Parking Spot deleted successfully");
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateParkingSport(
+            @PathVariable(value="id") UUID id,
+            @RequestBody @Valid ParkingSportDTO parkingSportDTO) {
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found");
+        }
+        var parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSportDTO, parkingSpotModel);
+        parkingSpotModel.setId(parkingSpotModelOptional.get().getId());
+        parkingSpotModel.setRegistrationDate(parkingSpotModelOptional.get().getRegistrationDate());
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
+    }
+
 }
