c32ae1fe1f1da19da9b4daca55242aba4db00105
Felipe D. Santos <lycan.felipe@gmail.com>
2022-08-31 20:20:16 -0300

    Pagenations and DateGlobal

--- Projects/parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ Projects/parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -2,6 +2,8 @@ package com.api.parkingcontrol.services;
 
 import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,8 +40,8 @@ public class ParkingSpotService {
         return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
     }
 
-    public List<ParkingSpotModel> findAll() {
-        return parkingSpotRepository.findAll();
+    public Page<ParkingSpotModel> findAll(Pageable pageable) {
+        return parkingSpotRepository.findAll(pageable);
     }
 
 
