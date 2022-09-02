cb88d16ed470e766af778f5844911256e3279325
Felipe D. Santos <lycan.felipe@gmail.com>
2022-09-02 05:54:33 -0300

    Roles

--- Projects/parking-control-api/src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
+++ Projects/parking-control-api/src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
@@ -3,12 +3,16 @@ package com.api.parkingcontrol.configs.security;
 import com.api.parkingcontrol.models.UserModel;
 import com.api.parkingcontrol.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
@@ -18,7 +22,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
         UserModel userModel = userRepository.findByUserName(userName)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with userName: " + userName));
-        return userModel;
+        return new User(userModel.getUserName(), userModel.getPassword(), true, true, true, true, userModel.getAuthorities());
     }
 
 }
--- Projects/parking-control-api/src/main/java/com/api/parkingcontrol/models/UserModel.java
+++ Projects/parking-control-api/src/main/java/com/api/parkingcontrol/models/UserModel.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -20,10 +21,16 @@ public class UserModel implements UserDetails, Serializable {
     private String userName;
     @Column(nullable = false)
     private String password;
+    @ManyToMany
+    @JoinTable(name = "TB_USER_ROLES",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<RoleModel> roles;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return this.roles;
     }
 
     @Override
