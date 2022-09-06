6666dc42f0a0fc2bd7ceb589c116bd6abed0b6e5
Felipe D. Santos <lycan.felipe@gmail.com>
2022-09-06 06:34:02 -0300

    Remove old project and add new compact

--- Projects/springngblog/src/main/java/com/owl/springngblog/config/IServiceWebSecurityConfigurer.java
+++
@@ -1,7 +0,0 @@
-package com.owl.springngblog.config;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
-public interface IServiceWebSecurityConfigurer {
-    void configure(HttpSecurity http) throws Exception;
-}
