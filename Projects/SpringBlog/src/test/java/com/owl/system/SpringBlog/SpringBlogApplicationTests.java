f24167a166df3c46cceca7d0b6d6c7f5a23093fb
Felipe D. Santos <lycan.felipe@gmail.com>
2022-09-06 06:38:04 -0300

    Init project SpringBlog

---
+++ Projects/SpringBlog/src/test/java/com/owl/system/SpringBlog/SpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owl.system.SpringBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
