ffbcf54d12d1f29ef17abfda6ae5876f0e4f8d64
Felipe D. Santos <lycan.felipe@gmail.com>
2022-09-20 15:50:27 -0300

    Passwor Encode with BCrypt

--- Projects/SpringBlog/src/main/java/com/owl/system/springblog/service/AuthService.java
+++ Projects/SpringBlog/src/main/java/com/owl/system/springblog/service/AuthService.java
@@ -4,6 +4,7 @@ import com.owl.system.springblog.dto.RegisterRequest;
 import com.owl.system.springblog.model.User;
 import com.owl.system.springblog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,11 +13,17 @@ public class AuthService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     public void signup(@RequestBody RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
