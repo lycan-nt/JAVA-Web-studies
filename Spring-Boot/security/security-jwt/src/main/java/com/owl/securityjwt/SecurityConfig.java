9f252eb51a586c1053104de1a88bc72352495067
Felipe D. Santos <lycan.felipe@gmail.com>
2022-09-23 07:14:55 -0300

    Spring Security with JWT authentication

--- Spring-Boot/security/security-jwt/src/main/java/com/owl/securityjwt/SecurityConfig.java
+++ Spring-Boot/security/security-jwt/src/main/java/com/owl/securityjwt/SecurityConfig.java
@@ -1,15 +1,33 @@
 package com.owl.securityjwt;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
 
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    @Lazy
+    private LoginFilter loginFilter;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -25,6 +43,32 @@ public class SecurityConfig {
                 .roles("ADMIN", "USER")
                 .build());
         return uds;
+    }
+
+    @Bean
+    public AuthenticationManager auhtAuthenticationManager(UserDetailsService userDetailsService) {
+        var dao = new DaoAuthenticationProvider();
+        dao.setUserDetailsService(userDetailsService);
+        return new ProviderManager(dao);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilterAt(loginFilter, BasicAuthenticationFilter.class);
+        http.addFilterAt(jwtFilter, BasicAuthenticationFilter.class);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.exceptionHandling()
+                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                    response.setStatus(HttpServletResponse.SC_ACCEPTED);
+                    response.getWriter().println(accessDeniedException.getMessage());
+                })
+                .authenticationEntryPoint((request, response, authException) -> {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.getWriter().println(authException.getMessage());
+                });
+        return http.build();
     }
 
 }
--- Spring-Boot/security/security-jwt/src/main/java/com/owl/securityjwt/TestController.java
+++ Spring-Boot/security/security-jwt/src/main/java/com/owl/securityjwt/TestController.java
@@ -1,10 +1,13 @@
 package com.owl.securityjwt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
     @GetMapping("/")
@@ -14,7 +17,8 @@ public class TestController {
 
     @GetMapping("/profile")
     @PreAuthorize("hasRole('USER')")
-    public String profile() {
+    public String profile(Authentication authentication) {
+        log.info("Username={}, roles={}", authentication.getPrincipal(), authentication.getAuthorities());
         return "profile page";
     }
 
