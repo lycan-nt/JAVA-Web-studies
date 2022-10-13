36d406cf53e5a6d2509c81ad71eec24fd3244a88
Felipe D. Santos <lycan.felipe@gmail.com>
2022-10-12 21:22:45 -0300

    TDD

---
+++ Tests/TDD/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -0,0 +1,25 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+
+public class Calculadora {
+
+	public int somar(int a, int b) {
+		int result = a + b;
+		return result;
+	}
+
+	public int subtrair(int a, int b) {
+		int result = a - b;
+		return result;
+	}
+
+	public int dividir(int a, int b) throws NaoPodeDividirPorZeroException {
+		if (b == 0) {
+			throw new NaoPodeDividirPorZeroException();
+		}
+		int result = a / b;
+		return result;
+	}
+
+}
---
+++ Tests/TDD/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -0,0 +1,70 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+
+public class CalculadoraTest {
+	
+	private Calculadora calc;
+	
+	@Before
+	public void setup() {
+		calc = new Calculadora();
+	}
+	
+	@Test
+	public void deveSomarDoisValores() {
+		//Cenario
+		int a = 5;
+		int b = 3;
+		
+		//Acao
+		int result = calc.somar(a, b);
+		
+		//Verificacao
+		Assert.assertEquals(8, result);
+	}
+	
+	@Test
+	public void deveSubtrairDoisValores() {
+		//Canario
+		int a = 8;
+		int b = 5;
+		
+		//Acao
+		int result = calc.subtrair(a, b);
+		
+		//Verificacao
+		Assert.assertEquals(3, result);
+	}
+	
+	@Test
+	public void deveDividirDoisValores() throws NaoPodeDividirPorZeroException {
+		//Cenario
+		int a = 6;
+		int b = 3;
+		
+		//Acao
+		int result = calc.dividir(a, b);
+		
+		//Verificacao
+		Assert.assertEquals(2, result);
+		
+	}
+	
+	@Test(expected = NaoPodeDividirPorZeroException.class)
+	public void deveLancarExcecaoAoDividirPorZero() throws NaoPodeDividirPorZeroException {
+		//Cenario
+		int a = 10;
+		int b = 0;
+		
+		//Acao
+		calc.dividir(a, b);
+		
+		//Verificacao
+	}
+
+}
