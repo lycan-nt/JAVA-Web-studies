0c7a048d9981c5d1cf447301b2d80a62075f195d
Felipe D. Santos <lycan.felipe@gmail.com>
2023-02-16 21:12:26 -0300

    Project Virtual Shop - Microservice Product init

---
+++ Projects/Virtual-Shop/product-service/src/test/java/com/owl/virtualshop/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owl.virtualshop.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
