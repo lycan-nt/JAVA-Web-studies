0e7849727f0d396edbdabd0eb537f13430c38061
Felipe D. Santos <lycan.felipe@gmail.com>
2023-03-01 23:32:29 -0300

    Connect in Eureka Server

---
+++ Projects/Virtual-Shop/discovery-server/src/main/java/com/owl/virtualshop/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.owl.virtualshop.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
--- Projects/Virtual-Shop/product-service/src/main/java/com/owl/virtualshop/productservice/ProductServiceApplication.java
+++ Projects/Virtual-Shop/product-service/src/main/java/com/owl/virtualshop/productservice/ProductServiceApplication.java
@@ -2,8 +2,12 @@ package com.owl.virtualshop.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//@EnableEurekaClient
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
