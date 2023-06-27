609b75cd00c27f91cc784d3a32cbc433b467b5e6
Felipe D. Santos <lycan.felipe@gmail.com>
2023-06-27 20:34:43 -0300

    ms-course hr-worker

---
+++ microservices/ms-course/hr-work/src/main/java/com/owl/hrwork/HrWorkApplication.java
@@ -0,0 +1,13 @@
+package com.owl.hrwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkApplication.class, args);
+	}
+
+}
---
+++ microservices/ms-course/hr-work/src/test/java/com/owl/hrwork/HrWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owl.hrwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
