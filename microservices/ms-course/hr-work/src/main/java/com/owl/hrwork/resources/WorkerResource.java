ca996deb97bfa81770c10bc32930dd76b8fb80b4
Felipe D. Santos <lycan.felipe@gmail.com>
2023-06-28 22:27:47 -0300

    Ribbon

--- microservices/ms-course/hr-work/src/main/java/com/owl/hrwork/resources/WorkerResource.java
+++ microservices/ms-course/hr-work/src/main/java/com/owl/hrwork/resources/WorkerResource.java
@@ -1,18 +1,28 @@
 package com.owl.hrwork.resources;
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.owl.hrwork.entities.Worker;
 import com.owl.hrwork.repositories.WorkerRepository;
 
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment environment;
 
 	@Autowired
 	private WorkerRepository repository;
@@ -25,6 +35,9 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		
+		logger.info("PORT: " + this.environment.getProperty("local.server.port"));
+		
 		Worker worker = this.repository.findById(id).get();
 		return ResponseEntity.ok(worker);
 	}
