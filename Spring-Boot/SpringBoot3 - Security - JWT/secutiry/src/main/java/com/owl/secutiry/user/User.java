aae39c1068b1c35bdb0017a0f663c060fdb924bf
Felipe D. Santos <lycan.felipe@gmail.com>
2023-06-30 14:19:31 -0300

    User Entity

--- Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/user/User.java
+++ Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/user/User.java
@@ -1,5 +1,6 @@
 package com.owl.secutiry.user;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,8 +10,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "_user")
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
     private String firstName;
     private String lastName;
