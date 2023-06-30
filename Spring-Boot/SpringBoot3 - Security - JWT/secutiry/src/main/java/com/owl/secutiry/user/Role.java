badcbac2c55d00d505f7bbd2165962a31ba5f3ae
Felipe D. Santos <lycan.felipe@gmail.com>
2023-06-30 14:31:08 -0300

    UserDatails

---
+++ Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/user/Role.java
@@ -0,0 +1,7 @@
+package com.owl.secutiry.user;
+public enum Role {
+
+    USER,
+    ADMIN
+
+}
--- Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/user/User.java
+++ Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/user/User.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -12,7 +17,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
@@ -21,5 +26,41 @@ public class User {
     private String lastName;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
