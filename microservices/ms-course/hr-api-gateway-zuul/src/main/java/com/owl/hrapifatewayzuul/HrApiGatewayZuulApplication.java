70a821a0065118a351310462ec70be549ce5dd0f
Felipe D. Santos <lycan.felipe@gmail.com>
2023-06-29 22:16:52 -0300

    Project hr-api-gateway-zull created

---
+++ microservices/ms-course/hr-api-gateway-zuul/src/main/java/com/owl/hrapifatewayzuul/HrApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.owl.hrapifatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+	}
+
+}
