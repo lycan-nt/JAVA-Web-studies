c23bcbca46ff270dd1b20ceec03d2fe409d98e2c
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-03 10:15:07 -0300

    Generate Token

--- Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/config/JwtService.java
+++ Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/config/JwtService.java
@@ -2,13 +2,20 @@ package com.owl.secutiry.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -17,6 +24,8 @@ public class JwtService {
    private KeyStore keyStore;
    @Value("${jks.pass}")
    private String jksPass;
+    @Value("${jwt.expiration}")
+    private String EXPIRATION_TIME;
 
    @PostConstruct
    public void init() {
@@ -36,6 +45,28 @@ public class JwtService {
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
        final Claims claims = extractAllClaims(token);
        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+       return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+       return Jwts
+               .builder()
+               .setClaims(extraClaims)
+               .setSubject(userDetails.getUsername())
+               .setIssuedAt(new Date(System.currentTimeMillis()))
+               .setExpiration(getTimeExpiration())
+               .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+               .compact();
+    }
+
+    private Date getTimeExpiration() {
+        return  Date.from(LocalDateTime.now()
+                        .plusMinutes(Long.parseLong(this.EXPIRATION_TIME))
+                        .atZone(ZoneId.systemDefault())
+                        .toInstant());
     }
 
     private Claims extractAllClaims(String token) {
