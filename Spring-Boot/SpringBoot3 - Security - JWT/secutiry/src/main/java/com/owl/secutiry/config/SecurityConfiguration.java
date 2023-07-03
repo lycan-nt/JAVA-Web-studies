2713f587c45accb7f54ad2df9d78757a4fe031ba
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-03 14:00:06 -0300

    Demo

--- Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/config/SecurityConfiguration.java
+++ Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/config/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("")
+                .requestMatchers("/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
