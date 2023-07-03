ab64befab50d012af18f450aa81184188a4c83dc
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-03 11:32:06 -0300

    AuthController

---
+++ Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/auth/AuthController.java
@@ -0,0 +1,29 @@
+package com.owl.secutiry.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+        @RequestBody RegisterRequest registerRequest
+    ) {
+
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody AuthenticationRequest authenticationRequest
+    ) {
+
+    }
+
+}
