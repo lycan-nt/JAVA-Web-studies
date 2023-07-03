7063128f2ead30da991e6caa1a32f8f3094e98aa
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-03 13:54:46 -0300

    Authenticate

--- Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/auth/AuthenticationService.java
+++ Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/auth/AuthenticationService.java
@@ -5,6 +5,8 @@ import com.owl.secutiry.user.Role;
 import com.owl.secutiry.user.User;
 import com.owl.secutiry.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,7 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest registerRequest) {
         var user = User.builder()
@@ -32,5 +35,16 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
+        this.authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getEmail(),
+                        authenticationRequest.getPassword()
+                )
+        );
+        var user = this.userRepository.findByEmail(authenticationRequest.getEmail()).orElseThrow();
+        var jwtToken = this.jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
