5a1b4cd132880f34ebfe0f56eff5cdeb1ae3df9d
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-03 11:41:13 -0300

    AuthenticationResponse

--- Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/auth/AuthController.java
+++ Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/auth/AuthController.java
@@ -1,6 +1,7 @@
 package com.owl.secutiry.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
