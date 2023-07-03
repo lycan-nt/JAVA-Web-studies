ccd8a0dabf12f7290ca24c2c89b1a02c0abb7418
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-03 12:30:18 -0300

    Register

--- Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/auth/AuthenticationService.java
+++ Spring-Boot/SpringBoot3 - Security - JWT/secutiry/src/main/java/com/owl/secutiry/auth/AuthenticationService.java
@@ -1,13 +1,34 @@
 package com.owl.secutiry.auth;
 
+import com.owl.secutiry.config.JwtService;
+import com.owl.secutiry.user.Role;
+import com.owl.secutiry.user.User;
+import com.owl.secutiry.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
     public AuthenticationResponse register(RegisterRequest registerRequest) {
+        var user = User.builder()
+                .firstName(registerRequest.getFirstName())
+                .lastName(registerRequest.getLastName())
+                .email(registerRequest.getEmail())
+                .password(this.passwordEncoder.encode(registerRequest.getPassword()))
+                .role(Role.USER)
+                .build();
+        this.userRepository.save(user);
+        var jwtToken = this.jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
