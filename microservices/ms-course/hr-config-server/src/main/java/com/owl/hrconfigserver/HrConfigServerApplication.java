227bff63965b91a67dd7f9ce58a15f7c860c6391
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-17 10:39:05 -0300

    configuring hr-config-server

--- microservices/ms-course/hr-config-server/src/main/java/com/owl/hrconfigserver/HrConfigServerApplication.java
+++ microservices/ms-course/hr-config-server/src/main/java/com/owl/hrconfigserver/HrConfigServerApplication.java
@@ -2,7 +2,10 @@ package com.owl.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+@EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
 
