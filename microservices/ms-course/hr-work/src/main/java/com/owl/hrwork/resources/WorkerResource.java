154b4bccb5a055cb541eb1a67d37622cd40d0875
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-17 11:54:27 -0300

    hr-worker as a config server client

--- microservices/ms-course/hr-work/src/main/java/com/owl/hrwork/resources/WorkerResource.java
+++ microservices/ms-course/hr-work/src/main/java/com/owl/hrwork/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,8 @@ import com.owl.hrwork.repositories.WorkerRepository;
 public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	@Value("${test.config}")
+	private String testConfig;
 	
 	@Autowired
 	private Environment environment;
@@ -38,6 +41,12 @@ public class WorkerResource {
 		logger.info("PORT: " + this.environment.getProperty("local.server.port"));
 		Worker worker = this.repository.findById(id).get();
 		return ResponseEntity.ok(worker);
+	}
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> configs() {		
+		logger.info("CONFIG: " + this.testConfig);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
