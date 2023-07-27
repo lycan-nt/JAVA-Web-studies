501c70a323243ba00df92704db3fd1af9b0b4fee
Felipe D. Santos <lycan.felipe@gmail.com>
2023-07-27 05:58:04 -0300

    Project hr-user created

---
+++ microservices/ms-course/hr-user/src/test/java/com/owl/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owl.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
