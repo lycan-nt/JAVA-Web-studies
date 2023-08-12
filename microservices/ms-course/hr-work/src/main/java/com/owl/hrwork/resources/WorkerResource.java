f6d3189aafb30256d55dcb326b495e0112210cad
Felipe D. Santos <lycan.felipe@gmail.com>
2023-08-12 12:08:29 -0300

    hr-worker container

--- microservices/ms-course/hr-work/src/main/java/com/owl/hrwork/resources/WorkerResource.java
+++ microservices/ms-course/hr-work/src/main/java/com/owl/hrwork/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -23,12 +22,8 @@ import com.owl.hrwork.repositories.WorkerRepository;
 public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
-	@Value("${test.config}")
-	private String testConfig;
-	
 	@Autowired
 	private Environment environment;
-
 	@Autowired
 	private WorkerRepository repository;
 	
@@ -47,7 +42,6 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> configs() {		
-		logger.info("CONFIG: " + this.testConfig);
 		return ResponseEntity.noContent().build();
 	}
 	
