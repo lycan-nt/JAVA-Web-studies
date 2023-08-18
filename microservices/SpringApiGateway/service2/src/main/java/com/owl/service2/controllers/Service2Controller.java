ade16cadf0e18df78e0e5209e4cca1cadca7e5ef
Felipe D. Santos <lycan.felipe@gmail.com>
2023-08-17 23:05:21 -0300

    API Gateway

--- microservices/SpringApiGateway/service2/src/main/java/com/owl/service2/controllers/Service2Controller.java
+++ microservices/SpringApiGateway/service2/src/main/java/com/owl/service2/controllers/Service2Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/services")
+@RequestMapping("/")
 public class Service2Controller {
 
     @GetMapping("/say")
