e504c8157ed6c425231b851e690b0b2faf7cbceb
Felipe D. Santos <lycan.felipe@gmail.com>
2023-09-26 06:50:22 -0300

    Customer Class

---
+++ Cursos/oracle-explore/exercises/src/duke/choise/Customer.java
@@ -0,0 +1,21 @@
+package duke.choise;
+
+public class Customer {
+	
+	private String name;
+	
+	public Customer() {}
+	
+	public Customer(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ Cursos/oracle-explore/exercises/src/duke/choise/ShopApp.java
@@ -0,0 +1,11 @@
+package duke.choise;
+
+public class ShopApp {
+
+	public static void main(String[] args) {
+		Customer c1 = new Customer();
+		c1.setName("Felipe D. Santos");
+		System.out.println("Hello " + c1.getName());
+	}
+	
+}
